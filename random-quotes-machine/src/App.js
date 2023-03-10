857c91e249003689e1e01e0ebfe7a2553257ddeb
unknown <roddrigo.benitez@gmail.com>
2023-03-10 13:32:13 -0300

    1part for quoteMachine

--- random-quotes-machine/src/App.js
+++ random-quotes-machine/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// this VAr is for colors the quote machine
+const colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857',
+  '#e7c142'
+];
+
+const QuoteMachine = ({ quote, handleNewQuote, bodyColor, buttonColor }) => (
+    <div id='quote-box'>
+      <p id='text'>{quote.text}</p>
+      <h2 id='author'>{quote.author}</h2>
+      <div class='actions'>
+        <button id='new-quote' class='button' onClick={handleNewQuote} style={{backgroundColor: buttonColor}}>
+          New Quotes
+        </button>
+        <a href='twitter.com/intent/tweet' target='_blank' id='tweet-quote' style={{backgroundColor: buttonColor}}>
+        Twiter
+        </a>
+      </div>
+    </div>
+  );
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteMachine />
     </div>
   );
 }
