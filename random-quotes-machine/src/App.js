666319eba5a503513e4f82a7bb4abe685cd42e63
unknown <roddrigo.benitez@gmail.com>
2023-03-10 14:49:52 -0300

    Add texts n btns

--- random-quotes-machine/src/App.js
+++ random-quotes-machine/src/App.js
@@ -1,4 +1,17 @@
+import React, {useState, useEffect} from 'react'
 import './App.css';
+
+const quoteData = [
+  {text:'The greatest glory in living lies not in never falling, but in rising every time we fall.', author: ' -Nelson Mandela'},
+  {text:"In the end, it's not the years in your life that count. It's the life in your years. ", author: '-Abraham Lincoln'},
+  {text:'Never let the fear of striking out keep you from playing the game.', author: '-Babe Ruth'},
+  {text:'Life is either a daring adventure or nothing at all.', author: '-Helen Keller'},
+  {text:"Many of life's failures are people who did not realize how close they were to success when they gave up.", author: ' -Thomas A. Edison'},
+  {text:'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.', author: ' -Dr. Seuss'},
+  {text: "Whoever is happy will make others happy too.", author: '-Anne Frank'},
+  {text:'Do not go where the path may lead, go instead where there is no path and leave a trail.', author: ' -Ralph Waldo Emerson'},
+  {text:'When you reach the end of your rope, tie a knot in it and hang on.', author: ' -Franklin D. Roosevelt'},
+]
 
 // this VAr is for colors the quote machine
 const colors = [
@@ -32,11 +45,22 @@ const QuoteMachine = ({ quote, handleNewQuote, bodyColor, buttonColor }) => (
     </div>
   );
 
+const getRandomIndex = () => Math.round(Math.random() * ((quoteData.length-1) - 0) + 0 );
+
 
 const App = () => {
+  const [quote, setQuote] = React.useState(quoteData[getRandomIndex()]);
+  //const [bgColor, setBgColor] = React.useState('color inicial');
+  const [bodyColor, setBodyColor] = React.useState('#e7c142');
+  const [buttonColor, setButtonColor] = React.useState('#e7c142');
+  const handleNewQuote = () => {
+    setQuote(quoteData[getRandomIndex()]);
+    setBodyColor(colors[getRandomIndex()]);
+    setButtonColor(colors[getRandomIndex()]);
+  }
   return (
-    <div className="App">
-      <QuoteMachine />
+    <div className="main" style={{backgroundColor: buttonColor}}>
+      <QuoteMachine quote={quote} handleNewQuote={handleNewQuote} buttonColor={buttonColor} bodyColor={bodyColor} />
     </div>
   );
 }
